94615be875d4b25a8202c9e42c42e6401b2781c4
Inik_21 <n.dzhunov@yahoo.com>
2020-04-21 07:39:17 +0300

    Add ProjectionsSeeder

--- Data/KinoPolis.Data/Seeding/ApplicationDbContextSeeder.cs
+++ Data/KinoPolis.Data/Seeding/ApplicationDbContextSeeder.cs
@@ -29,6 +29,7 @@
                               new SettingsSeeder(),
                               new FilmsSeeder(),
                               new CinemasSeeder(),
+                              new ProjectionsSeeder(),
                           };
 
             foreach (var seeder in seeders)
--- Data/KinoPolis.Data/Seeding/ProjectionsSeeder.cs
+++ Data/KinoPolis.Data/Seeding/ProjectionsSeeder.cs
@@ -1,15 +1,40 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace KinoPolis.Data.Seeding
+﻿namespace KinoPolis.Data.Seeding
 {
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+
+    using KinoPolis.Data.Models;
+
     public class ProjectionsSeeder : ISeeder
     {
-        public Task SeedAsync(ApplicationDbContext dbContext, IServiceProvider serviceProvider)
+        public async Task SeedAsync(ApplicationDbContext dbContext, IServiceProvider serviceProvider)
         {
-            throw new NotImplementedException();
+            var projections = new List<(int Price, DateTime Time, int FilmId, int CinemaId)>
+            {
+                (18, new DateTime(2019, 12, 9, 17, 0, 0), 1, 2),
+                (14, new DateTime(2020, 2, 12, 11, 0, 0), 2, 1),
+                (8, new DateTime(2020, 3, 17, 13, 30, 0), 3, 3),
+            };
+            if (dbContext.Projections.Any())
+            {
+                return;
+            }
+            else
+            {
+                foreach (var projection in projections)
+                {
+                    await dbContext.Projections.AddAsync(new Projection
+                    {
+                        Price = projection.Price,
+                        Time = projection.Time,
+                        FilmId = projection.FilmId,
+                        CinemaId = projection.CinemaId,
+                    });
+                }
+            }
         }
     }
 }
