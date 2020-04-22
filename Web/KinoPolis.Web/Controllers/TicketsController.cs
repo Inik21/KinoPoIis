5f7c7387210cfbb827a47fbbd3eeb818caff5e1b
Inik_21 <n.dzhunov@yahoo.com>
2020-04-22 07:37:59 +0300

    Add TicketsController

---
+++ Web/KinoPolis.Web/Controllers/TicketsController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace KinoPolis.Web.Controllers
+{
+    public class TicketsController : BaseController
+    {
+        public IActionResult ById(int ticketSeat, int id)
+        {
+            return this.View();
+        }
+    }
+}
