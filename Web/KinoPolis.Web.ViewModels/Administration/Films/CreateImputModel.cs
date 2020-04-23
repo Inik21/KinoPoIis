f6f3f0a0c60710adb536088a0f4187f99f289caf
Inik_21 <n.dzhunov@yahoo.com>
2020-04-23 07:22:48 +0300

    Add Administrator panel. Add dashboard. Add FilmsController. Add CreateImputModel

---
+++ Web/KinoPolis.Web.ViewModels/Administration/Films/CreateImputModel.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+
+namespace KinoPolis.Web.ViewModels.Administration.Films
+{
+    public class CreateImputModel
+    {
+        [Required(ErrorMessage ="The field is required")]
+        [RegularExpression(@"^[a-zA-Z0-9:]*$", ErrorMessage ="The field should contain only numbers, letters and :")]
+        public string Name { get; set; }
+
+        [Required(ErrorMessage = "The field is required")]
+        [RegularExpression(@"^[a-zA-Z/]*$", ErrorMessage = "The field should contain only letters and /")]
+        public string Category { get; set; }
+
+        [Required(ErrorMessage = "The field is required")]
+        [RegularExpression(@"^[a-zA-Z]*$", ErrorMessage = "The field should contain only letters")]
+        public string Country { get; set; }
+
+        [Required(ErrorMessage = "The field is required")]
+        [RegularExpression(@"^[0-9]*$",ErrorMessage = "The field should contain only numbers")]
+        public int Year { get; set; }
+
+        [Required(ErrorMessage = "The field is required")]
+        [RegularExpression(@"^[0-9]*$", ErrorMessage = "The field should contain only numbers")]
+        public int Lenght { get; set; }
+
+        [Required(ErrorMessage = "The field is required")]
+        public string Description { get; set; }
+
+        [Required(ErrorMessage = "The field is required")]
+        public string ImgUrl { get; set; }
+    }
+}
---
+++ Web/KinoPolis.Web/Areas/Administration/Controllers/FilmsController.cs
@@ -0,0 +1,33 @@
+﻿
+using KinoPolis.Common;
+using KinoPolis.Web.ViewModels.Administration.Films;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace KinoPolis.Web.Areas.Administration.Controllers
+{
+    public class FilmsController : AdministrationController
+    {
+        [Authorize(Roles = GlobalConstants.AdministratorRoleName)]
+        public IActionResult Create()
+        {
+            return this.View();
+        }
+
+        [Authorize(Roles = GlobalConstants.AdministratorRoleName)]
+        [HttpPost]
+        public IActionResult Create(CreateImputModel input)
+        {
+            if (!this.ModelState.IsValid)
+            {
+                return this.View(input);
+            }
+
+            return this.Redirect("/");
+        }
+    }
+}
