fea84e9304bc57b3241ac4fc42b1a54d96da6d6f
Inik_21 <n.dzhunov@yahoo.com>
2020-04-23 08:52:57 +0300

    Add CinemasController

---
+++ Web/KinoPolis.Web/Areas/Administration/Controllers/CinemasController.cs
@@ -0,0 +1,26 @@
+﻿using KinoPolis.Common;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace KinoPolis.Web.Areas.Administration.Controllers
+{
+    public class CinemasController : AdministrationController
+    {
+        [Authorize(Roles = GlobalConstants.AdministratorRoleName)]
+        public IActionResult Create()
+        {
+            return this.View();
+        }
+
+        [HttpPost]
+        [Authorize(Roles = GlobalConstants.AdministratorRoleName)]
+        public IActionResult Create()
+        {
+            return this.View();
+        }
+    }
+}
